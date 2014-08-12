d5e57ec09b9355098624787ed32d6cb2580cc00e
mattanja <mattanja.kern@gmail.com>
2014-08-12 19:45:21 +0200

    tracking code

--- app/js/config-sample.js
+++ app/js/config-sample.js
@@ -1,10 +1,18 @@
+'use strict';
 
 /**
  * Example configuration file - should be copied to config.js file on server.
  */
 
-angular.module('myApp').config(['appConfiguration', function(appConfiguration) {
+angular.module('myApp')
+.config(['appConfiguration', function(appConfiguration) {
 	appConfiguration.tracking = true;
 	appConfiguration.gaTrackingId = 'UA-12345678-9';
 	appConfiguration.trelloApiKey = '1234567890';
-}]);
+}])
+.run(['appConfiguration', function(appConfiguration) {
+	if (appConfiguration.tracking) {
+		ga('create', appConfiguration.gaTrackingId, 'auto');
+	}
+}])
+;
